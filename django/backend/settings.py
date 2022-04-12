bc2dd6a0078c72978a0f0a876f8ad58948d87a71
Brian Rogador <brianjos@ualberta.ca>
2022-04-11 23:15:14 -0600

    Add part 2 staging

--- django/backend/settings.py
+++ django/backend/settings.py
@@ -132,3 +132,5 @@ MEDIA_ROOT = BASE_DIR / "mediafiles"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
